d8403e5fd9043b1442d425162ff4d0df226c8518
zwimer <zwimer@gmail.com>
2023-12-26 16:44:53 -0500

    up-number bug fix and allow --force

--- manga/tools/up_number.py
+++ manga/tools/up_number.py
@@ -5,7 +5,7 @@ import sys
 from manga.utils import split_on_num, mv
 
 
-def up_number(file: Path, number: int, yes: bool) -> bool:
+def up_number(file: Path, number: int, yes: bool, force: bool) -> bool:
     absolute: Path = file.absolute()
     file = file.resolve()
     # Santiy check
@@ -18,7 +18,8 @@ def up_number(file: Path, number: int, yes: bool) -> bool:
     # Calculate new name
     left, old_n, right = split_on_num(file.name)
     assert len(left) != 0, f"Something is wrong with the name of {file}"
-    assert old_n <= number, f"New number {number} is not greater than old number {old_n}"
+    if not force:
+        assert old_n < number, f"New number {number} is not greater than old number {old_n}"
     n_str = str(int(number) if round(number) == number else number)
     new: Path = file.parent / (left + n_str + right)
     # Prompt user
@@ -38,6 +39,9 @@ def main(prog: str, *args: str) -> bool:
     parser = argparse.ArgumentParser(prog=Path(prog).name)
     parser.add_argument(
         "-y", "--yes", action="store_true", help="Automatically accept changes; will never prompt the user"
+    )
+    parser.add_argument(
+        "-f", "--force", action="store_true", help="Allow changes even if the chapter number does not increase"
     )
     parser.add_argument("file", type=Path, help="The file to increase the number of")
     parser.add_argument("number", type=float, help="The number to increment file to")
